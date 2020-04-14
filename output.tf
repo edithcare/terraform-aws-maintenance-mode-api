55699da7dc2f30abc2ad5bc8585f0434e3e08baf
David Robakowski <david.robakowski@synlay.com>
2020-04-14 11:08:28 +0200

    Include new output aws_api_gateway_domain_names

--- output.tf
+++ output.tf
@@ -1,5 +1,15 @@
 output environments {
-  value = var.environments
-
   description = "The map with created environments."
+  value       = var.environments
+}
+
+output aws_api_gateway_domain_names {
+  description = "The map with a mapping from environment to its associated API Gateway endpoint."
+  value = {
+    for environment in keys(var.environments) :
+    environment => {
+      domain_name = aws_api_gateway_domain_name.maintenance_api[environment].regional_domain_name
+      zone_id     = aws_api_gateway_domain_name.maintenance_api[environment].regional_zone_id
+    }
+  }
 }
