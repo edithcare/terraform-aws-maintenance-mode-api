eb8ea6db580839791995b82ec79b28e5ce08b5ee
Daniel Tinney <dnltinney@gmail.com>
2021-04-15 14:19:29 +0200

    add missing resource

--- main.tf
+++ main.tf
@@ -19,6 +19,12 @@ resource "aws_api_gateway_rest_api" "maintenance_api" {
   }
 }
 
+resource "aws_api_gateway_resource" "maintenance_api" {
+  rest_api_id = aws_api_gateway_rest_api.maintenance_api.id
+  parent_id   = aws_api_gateway_rest_api.maintenance_api.root_resource_id
+  path_part   = "/"
+}
+
 resource "aws_api_gateway_deployment" "maintenance_api" {
   rest_api_id = aws_api_gateway_rest_api.maintenance_api.id
 
